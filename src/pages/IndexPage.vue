0ed1f38923175c226b643a5ec3055ee0cf2b362d
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2026-05-23 12:01:25 -0300

    Add data about creator

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,11 @@
 <template>
   <q-page class="flex flex-center column q-pa-xl">
     <div class="text-center">
-      <q-icon name="lock" size="100px" color="primary" />
-      <div class="text-h1 text-weight-bold text-primary q-my-md">Stego</div>
-      <div class="text-subtitle1 text-grey-6 q-mb-xl">
+      <q-icon name="image_search" size="100px" color="primary" />
+      <div class="text-h1 text-weight-bold text-primary q-my-md">Stego Mania</div>
+      <p class="text-subtitle1 text-grey-6 q-mb-xl">
         Oculte mensagens secretas dentro de imagens
-      </div>
+      </p>
 
       <div class="row q-gutter-md justify-center">
         <q-btn
@@ -29,6 +29,10 @@
           style="min-width: 220px"
         />
       </div>
+    </div>
+    <div class="q-mt-lg q-pa-md text-center text-grey-7">
+      <p>Desenvolvido por J. L. Gregório</p>
+      <p><a href="https://www.jlgregorio.com.br" target="_blank"> www.jlgregorio.com.br</a></p>
     </div>
   </q-page>
 </template>
